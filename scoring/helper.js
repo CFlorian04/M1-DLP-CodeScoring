87edfa9d2606a8a53f87369014f430f36da297e3
bechir <118394171+bechir-Lahouel@users.noreply.github.com>
2024-03-14 22:02:12 +0100

    implementation de allDeclaredIsUsed reste a bien verifier

--- scoring/helper.js
+++ scoring/helper.js
@@ -1,8 +1,36 @@
 const { forEach } = require("lodash");
 
-exports.allDeclaredIsUsed = (ast) => {
-    return 1;
+ exports.allDeclaredIsUsed = (ast)=> {
+    const declaredVariables = new Set();
+    const usedVariables = new Set();
+
+    for (const node of ast) {
+        if (node.type === 'variableDeclaration') {
+            declaredVariables.add(node.variableName);
+        } else if (node.type === 'variableAffectation') {
+            usedVariables.add(node.variableName);
+        }
+    }
+
+    if (declaredVariables.size === 0) {
+        return 0;
+    } else {
+        const score = Math.round(Math.min(10, Math.max(0, 10 * usedVariables.size / declaredVariables.size)) * 100) / 100;
+        return score;
+    }
 }
+
+
+
+// // Exemple d'utilisation
+// const pythonCode = `
+// x = 10
+// y = 20
+// print(x)
+// `;
+
+// console.log(allDeclaredAreUsed(pythonCode)); // true, toutes les variables déclarées sont utilisées
+
 
 exports.allUsedIsDeclared = (ast) => {
     return 1;
